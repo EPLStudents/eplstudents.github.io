8f0aab31a26eceedebdd0625d984536824e83908
hokkaydo <hokkaydo@users.noreply.github.com>
2024-06-02 23:30:22 +0200

    Add site content back

--- gen_static.sh
+++ gen_static.sh
@@ -22,10 +22,11 @@ sleep 2
 port=$(lsof -i -P -n | grep LISTEN  | grep eplstuden | cut -d: -f2 | cut -d' ' -f1)
 echo "Port: $port"
 
-#for route in $ROUTES; do
-#	mkdir -p static$route
-#	curl http://localhost:$port$route -o static$route/index.html
-#done
+for route in $ROUTES; do
+	mkdir -p static$route
+	curl http://localhost:$port$route -o static$route/index.html
+done
+
 jobs=$(jobs -p | cut -d+ -f2 | cut -d' ' -f2)
 if [ $jobs ]; then 
 	kill $jobs
